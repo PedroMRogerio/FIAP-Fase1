2c706bd35e25b36b234ff48871f0fbd6ff01737e
Pedro Moraes <pmoraes.rogerio@gmail.com>
2025-11-19 14:37:19 -0300

    8: Criação da segunda rota de WebScrapping

--- app.py
+++ app.py
@@ -89,12 +89,50 @@ def scrape_title():
        description: URL of the web page
     responses:
      200:
-        description: Web apage title
+        description: Web page title
     """
     url = request.args.get("url")
     if not url:
         return jsonify({"error": "URL is required"}), 400
     return get_title(url)
+
+def get_content(url):
+    try:
+        response = requests.get(url)
+        soup = BeautifulSoup(response.text, "html.parser")
+
+        headers = []
+        for header_tag in ['h1', 'h2', 'h3']:
+            for header in soup.find_all(header_tag):
+                headers.append(header.get_text(strip=True))
+
+        paragraphs = [p.get_text(strip=True) for p in soup.find_all('p')]
+        return jsonify({"headers": headers, "paragraphs": paragraphs})
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+
+@app.route('/scrape/content', methods=["GET"])
+@auth.login_required
+def scrape_content():
+    """
+    Extract headers and paragraphs froam a web page provided by the URL.
+    ---
+    security:
+     - BasicAuth: []
+    parameters:
+     - name: url
+       in: query
+       type: string
+       required: true
+       description: URL of the web page
+    responses:
+     200:
+        description: Web page content
+    """
+    url = request.args.get("url")
+    if not url:
+        return jsonify({"error": "URL is required"}), 400
+    return get_content(url)
 
 if __name__ == "__main__":
     app.run(debug=True)
