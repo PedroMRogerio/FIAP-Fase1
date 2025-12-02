69b4f5b4bd8b4809039a017c827195a568a3275b
Pedro Moraes <pmoraes.rogerio@gmail.com>
2025-12-02 14:31:14 -0300

    11: Criação do banco de dados

--- Receitas/appReceitas.py
+++ Receitas/appReceitas.py
@@ -1,5 +1,6 @@
 from flasgger import Swagger
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 import config
 
@@ -7,10 +8,27 @@ app = Flask(__name__)
 
 app.config.from_object(config)
 
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    password = db.Column(db.String(120), nullable=False)
+    
+class Recipe(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(120), nullable=False)
+    ingredients = db.Column(db.Text, nullable=False)
+    time_minutes = db.Column(db.Integer, nullable=False)
+    
 @app.route("/")
 def home():
     return "Página Inicial"
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    #app.run(debug=True)
+    with app.app_context():
+        db.create_all()
+        print('Branco de dados criado!')
+    
